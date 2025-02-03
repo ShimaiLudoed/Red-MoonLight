722a6b4e096d9f38c8c6ad727ed62a5faf9bf48f
ShimaiLudoed <MikhaiilShvets@yandex.ru>
2025-02-04 00:11:53 +0300

    Refactor:
     delete useless Using, delete some monobehavior

--- Red Moonlight/Assets/Source/Player/Core/PlayerController.cs
+++ Red Moonlight/Assets/Source/Player/Core/PlayerController.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
+public class PlayerController 
 {
     // Start is called before the first frame update
     void Start()
