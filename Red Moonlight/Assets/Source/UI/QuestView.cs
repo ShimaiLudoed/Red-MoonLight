e6c34cdf70bd5e20be7101856451233146c674de
ITHUB\Teacher <312>
2025-03-17 19:55:34 +0300

    fix

--- Red Moonlight/Assets/Source/UI/QuestView.cs
+++ Red Moonlight/Assets/Source/UI/QuestView.cs
@@ -1,11 +1,12 @@
 using Data;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestView : ItemView<QuestSO>
 {
-    public override void Init(QuestSO data)
+    public override void Init(QuestSO data, Action callback = null)
     {
         throw new System.NotImplementedException();
     }
